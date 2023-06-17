e5e62881ea6813c750f67b1e4c9e7e17d53f83fa
szy-kenn <johnkennethvelano@gmail.com>
2023-06-17 11:59:57 +0800

    refactored skyscanner api integration

---
+++ src/skyscanner.py
@@ -0,0 +1,68 @@
+import requests, requests_cache
+
+# caching api responses (cached for 1 day)
+requests_cache.install_cache('api_cache', expire_after=86400) 
+
+def request(origin, destination, departure_date) -> tuple:
+
+    url = "https://skyscanner44.p.rapidapi.com/search"
+    querystring = {
+        "adults": 1,
+        "origin": origin,
+        "destination": destination,
+        "departureDate": departure_date,
+        "currency":"PHP",
+        "locale":"en-GB",
+        "market":"PH"}
+
+    headers = {
+	    "X-RapidAPI-Key": "fd32d46d08msh3e5a5981b4f2bc5p13a8a7jsn21617cfcf8cf", # unique key
+	    "X-RapidAPI-Host": "skyscanner44.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    skyscanner_response = response.json()
+
+    try:
+        total_results = skyscanner_response['context']['totalResults']
+        if total_results == 0:
+            return (0, ('No Results Found.', 'Try a different departure date.'))
+    except:
+        return (0, ('Bad Request.', 'Try a different date or check all your inputs again.'))
+
+    best = skyscanner_response['itineraries']['buckets'][0]
+
+    available_flights = {
+        "best" : []
+    }
+
+    i = 0
+    while True:
+        try:
+            result = best['items'][i]
+            key = 'best'
+        except:
+            break
+    
+        available_flights[key].append({})
+        available_flights[key][i]['arrival_time'] = result['legs'][0]['arrival']
+        available_flights[key][i]['departure_time'] = result['legs'][0]['departure']  
+        available_flights[key][i]['segments'] = result['legs'][0]['segments']  
+        available_flights[key][i]['duration_in_mins'] = result['legs'][0]['durationInMinutes']  
+        available_flights[key][i]['stop_count'] = result['legs'][0]['stopCount']  
+        available_flights[key][i]['price'] = result['price']['raw'] 
+        i+=1
+
+    return (i, available_flights)
+
+    # skyscanner_response['itineraries']['buckets'][0/1/2] => best / cheapest / fastest
+    # => ['items'][0-n] results in best/cheaptest/fastest
+    # => ['legs']
+
+    # arrival_time = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['arrival'] 
+    # departure_time = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['departure']
+    # destination = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['destination']
+    # duration_in_mins = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['durationInMinutes']
+    # segments = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['segments'] # stops
+    # stop_count = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['stopCount']
+    # price = skyscanner_response['itineraries']['buckets'][0]['items'][0]['price']['raw']
--- src/views.py
+++ src/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, url_for, redirect, jsonify, session, flash
 from . import mysql
 import json
-import requests, requests_cache
+from . import skyscanner
 
 view = Blueprint('view', __name__)
 
@@ -24,104 +24,27 @@ def flights():
     session['form_part_one']['from-json'] = json.loads(session['form_part_one']['from-json']);
 
     # return session['form_part_one']
-    '''
-    For Skyscanner API integration:
-    '''
-    url = "https://skyscanner44.p.rapidapi.com/search"
-    querystring = {
-        "adults":session['form_part_one']['passenger-adult'],
-        "origin":session['form_part_one']['to-json']['iata'],
-        "destination":session['form_part_one']['from-json']['iata'],
-        "departureDate":session['form_part_one']['departure-date'],
-        "currency":"PHP",
-        "locale":"en-GB",
-        "market":"PH"}
 
-    headers = {
-        "X-RapidAPI-Key": "a5ac9eda9cmsh534816737785f1bp15fabdjsn04779416bc84",
-        "X-RapidAPI-Host": "skyscanner44.p.rapidapi.com"
-    }
-
-    requests_cache.install_cache('api_cache', expire_after=200)
-
-    response = requests.get(url, headers=headers, params=querystring)
-    skyscanner_response = response.json()
+    origin = session['form_part_one']['to-json']['iata'],
+    destination = session['form_part_one']['from-json']['iata'],
+    departure_date = session['form_part_one']['departure-date'],
     
-    # return skyscanner_response
-
-    total_results = 0
-
-    try:
-        total_results = skyscanner_response['context']['totalResults']   
-    except:
-        return render_template('flights.html', form_part_one=session['form_part_one'], result_count=0, error_message=['Bad Request.', 'Try a different date or check all your inputs again.'])
-
-    # skyscanner_response['itineraries']['buckets'][0/1/2] => best / cheapest / fastest
-        # => ['items'][0-n] results in best/cheaptest/fastest
-        # => ['legs']
-
-    if total_results == 0:
-        return render_template('flights.html', form_part_one=session['form_part_one'], result_count=0, error_message=['No Results Found.', 'Try a different departure date.'])
-
-    arrival_time = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['arrival'] 
-    departure_time = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['departure']
-    destination = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['destination']
-    duraiton_in_mins = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['durationInMinutes']
-    segments = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['segments'] # stops
-    stop_count = skyscanner_response['itineraries']['buckets'][0]['items'][0]['legs'][0]['stopCount']
-    price = skyscanner_response['itineraries']['buckets'][0]['items'][0]['price']['raw']
-
-    best = skyscanner_response['itineraries']['buckets'][0]
-    cheapest = skyscanner_response['itineraries']['buckets'][1]
-    fastest = skyscanner_response['itineraries']['buckets'][2]
-
-    available_flights = {
-        "best" : [],
-        "cheapest" : [],
-        "fastest" : []
-    }
-
-    for h in range(3):
-        i = 0
-        
-        while True:
-
-            try:
-                if (h == 0):
-                    result = best['items'][i]
-                    key = 'best'
-                elif (h == 1):
-                    result = cheapest['items'][i]
-                    key = 'cheapest'
-                else:
-                    result = fastest['items'][i]
-                    key = 'fastest'
-            except:
-                break
-
-            print(key)
-        
-            available_flights[key].append({})
-            available_flights[key][i]['arrival_time'] = result['legs'][0]['arrival']
-            available_flights[key][i]['departure_time'] = result['legs'][0]['departure']  
-            available_flights[key][i]['segments'] = result['legs'][0]['segments']  
-            available_flights[key][i]['duration_in_mins'] = result['legs'][0]['durationInMinutes']  
-            available_flights[key][i]['stop_count'] = result['legs'][0]['stopCount']  
-            available_flights[key][i]['price'] = result['price']['raw'] 
-            i+=1
-
-
-    # available_flights = {
-    #     "best" : {
-    #         "arrival_time" : best['items'][0]['legs'][0]['arrival'],
-    #         "departure_time" : best['items'][0]['legs'][0]['departure'],
-    #         "destination" : best['items'][0]['legs'][0]['destination'],
-    #         "duration_in_mins" : best['items'][0]['legs'][0]['durationInMinutes'],
-    #         # "segments" : best['items'][0]['legs'][0]['segments'], # stops
-    #         "stop_count" : best['items'][0]['legs'][0]['stopCount'],
-    #         "price" : best['items'][0]['price']['raw'] 
-    #     }
-    # }
-
+    available_flights = skyscanner.request(origin, destination, departure_date)
     # return jsonify(available_flights)
-    return render_template('flights.html', form_part_one=session['form_part_one'], available_flights=available_flights, result_count=i)
+
+    if available_flights[0] == 0:
+        return render_template('flights.html', form_part_one=session['form_part_one'], result_count=0, error_message=available_flights[1])
+
+    return render_template('flights.html', form_part_one=session['form_part_one'], available_flights=available_flights[1], result_count=available_flights[0])
+
+@view.route('/passenger-details')
+def passenger_details():
+    return render_template('passenger-details.html')
+
+@view.route('/seats')
+def seats():
+    return render_template('seats.html')
+
+@view.route('/confirmation')
+def confirmation():
+    return render_template('ticket.html')
